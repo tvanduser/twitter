9dfe72a33113da09ecedc4072563b4d8f326ff69
tvanduser <108898340+tvanduser@users.noreply.github.com>
2023-11-10 20:39:53 -0600

    Update twitter.py
    added a save and loading tweets feature using pickle

--- twitter.py
+++ twitter.py
@@ -1,58 +1,76 @@
+import pickle
+from tweet import Tweet
 
-from tweet import Tweet  # Assuming the Tweet class is in a separate file named Tweet.py
-#make a new tweet
+# Function to save tweets to a file
+def save_tweets(tweets):
+    with open("twitter.dat", "wb") as file:
+        pickle.dump(tweets, file)
+
+# Function to load tweets from a file
+def load_tweets():
+    try:
+        with open("twitter.dat", "rb") as file:
+            tweets = pickle.load(file)
+        return tweets
+    except FileNotFoundError:
+        return []
+
+# Function to make a new tweet
 def make_tweet(tweets):
-    author = input("Enter your name: ")
-    text = input("Enter your tweet (140 characters max): ")
-    #make sure tweet is correct length 
+    author = input("\nWhat is your name? ")
+    text = input("What would you like to tweet? ")
+    
     while len(text) > 140:
         print("Tweet is too long. Please limit your tweet to 140 characters.")
-        text = input("Enter your tweet (140 characters max): ")
-    #post the tweet by adding it to the tweets array 
+        text = input("Enter your tweet: ")
+
     new_tweet = Tweet(author, text)
     tweets.append(new_tweet)
-    print("Tweet successfully posted!")
-#view the recent tweets 
+    save_tweets(tweets)  # Save tweets after adding a new tweet
+    print("Your tweet has been saved.")
+
+# Function to view recent tweets
 def view_recent_tweets(tweets):
     if not tweets:
         print("No tweets available.")
     else:
         print("\nRecent Tweets:")
         print("------------------------")
-        for tweet in tweets[-5:][::-1]:  # Display the five most recent tweets in reverse order
+        for tweet in tweets[-5:][::-1]:
             tweet.display_tweet()
             print("\n")
-            
-#search through the tweets 
+
+# Function to search through tweets
 def search_tweets(tweets):
     if not tweets:
         print("No tweets available.")
     else:
-        search_text = input("Enter the text to search for in tweets: ")
-        #convert everything to lowercase and then searching for matches withing the tweets array 
+        search_text = input("What would you like to search for? ")
         matching_tweets = [tweet for tweet in tweets if search_text.lower() in tweet.get_text().lower()]
-        #nothing matches: this is what happens 
+        
         if not matching_tweets:
             print("No tweets found with the specified search text.")
         else:
-            print("Matching Tweets:")
+            print("\nSearch Results")
+            print("------------------------")
             for tweet in matching_tweets:
                 tweet.display_tweet()
-                print("------------------------")
+                print("\n")
+                
 
+# Main function
 def main():
-    tweets = []
+    tweets = load_tweets()
 
     while True:
-        #display the menu 
         print("\nTweet Menu")
         print("-----------")
         print("1. Make a Tweet")
         print("2. View Recent Tweets")
         print("3. Search Tweets")
         print("4. Quit")
-        #get what the user wants to do 
-        choice = input("Enter your choice (1-4): ")
+
+        choice = input("What would you like to do? ")
 
         if choice == "1":
             make_tweet(tweets)
@@ -61,10 +79,11 @@ def main():
         elif choice == "3":
             search_tweets(tweets)
         elif choice == "4":
-            print("Exiting the program. Goodbye!")
+            save_tweets(tweets)  # Save tweets before exiting
+            print("Thank you for using the Tweet Manager!")
             break
         else:
             print("Invalid choice. Please enter a number between 1 and 4.")
-#run the programm by getting it to go into main 
+
 if __name__ == "__main__":
     main()
